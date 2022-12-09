64670fd8170be5fae6bdda9c2f2223ee091659e0
kip8980 <cheruiyotkiptooelkan@gmail.com>
2022-12-09 03:06:46 -0800

    Hello world

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print a lne of code using puts
+ *
+ * Return: o
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
