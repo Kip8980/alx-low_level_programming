63813fb1d0a949479acd1e3432d23effcc53d732
kip8980 <cheruiyotkiptooelkan@gmail.com>
2022-12-15 00:06:59 -0800

    Task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - print _putchar
+ * main - Print _putchar
  * Return: 0
-*/
+ */
 int main(void)
 {
 	_putchar('_');
@@ -14,5 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
